e489549505ed60bacb83bc5f2a84f58a10876ef5
Rodrigo Albornoz <rodrigo.albornoz.f@gmail.com>
2025-05-27 11:05:41 -0300

    ts version

--- dist/vars.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.KAFKAJS_NO_PARTITIONER_WARNING = exports.KAFKA_CLIENT_ID = exports.KAFKA_CONSUMER_GROUP_ID = exports.KAFKA_PRODUCE_TOPIC = exports.KAFKA_READ_TOPIC = exports.KAFKA_BROKER = void 0;
-exports.KAFKA_BROKER = 'host.docker.internal:9094';
-exports.KAFKA_READ_TOPIC = 'route.new-direction';
-exports.KAFKA_PRODUCE_TOPIC = 'route.new-position';
-exports.KAFKA_CONSUMER_GROUP_ID = 'simulator-v2';
-exports.KAFKA_CLIENT_ID = 'simulator-v2';
-exports.KAFKAJS_NO_PARTITIONER_WARNING = 1;
